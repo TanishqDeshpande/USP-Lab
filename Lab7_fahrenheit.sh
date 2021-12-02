c1952b913f4f968d85a17aceba6aadadc06a73c4
TanishqDeshpande <71491689+TanishqDeshpande@users.noreply.github.com>
2021-12-02 10:29:26 +0530

    Update Lab7_fahrenheit.sh

--- Lab7_fahrenheit.sh
+++ Lab7_fahrenheit.sh
@@ -1,3 +1,6 @@
-read -p "Enter the temperature in Fahrenheit: " f
-c=$(echo "scale=2;(5/9)*($f-32)"|bc)
+echo "Enter the temperature in Fahrenheit: " 
+read f
+v1=`expr $f - 32`
+v2=`expr 5/9`
+c=`echo $v1 \* $v2 | bc`
 echo "The temperature in Celsius is: $c"
