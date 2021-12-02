1d7b00a40877b37893b82a57666abbfb5264ab1f
TanishqDeshpande <71491689+TanishqDeshpande@users.noreply.github.com>
2021-12-02 10:12:26 +0530

    Delete leargest.sh

--- leargest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-read -p "Enter three numbers: " a b c
-if [ $a -ge $b -a $a -ge $c ] ; then
-echo "$a is the largest"
-elif [ $b -ge $a -a $b -ge $c ] ; then
-echo "$b is the largest"
-else
-echo "$c is the largest"
-fi
